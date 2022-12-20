303385387cd231dcead3514f7f887ca5300a8a4e
Steve Richter <steve@nerdbrain.com>
2022-12-20 01:25:21 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,13 @@ if [ -z "$SOURCE_DIR" ]; then
   exit 1
 fi
 
+EXCLUDE=""
+if [ -n "$EXCLUDE_LIST" ]; then
+  EXCLUDE="--excludeDirRegex '$EXCLUDE_LIST'"
+fi
+
 b2 authorize-account ${B2_APPKEY_ID} ${B2_APPKEY}
 
-b2 sync --delete --replaceNewer ${SOURCE_DIR} ${B2_BUCKET}
-
-# TO-DO: Delete old versions of updated files because that's what
-# we're already using GitHub for... but B2 doesn't make this easy:
-# https://github.com/Backblaze/B2_Command_Line_Tool/issues/324
+b2 sync ${EXCLUDE} ${SOURCE_DIR} ${B2_BUCKET}
 
 b2 clear-account
